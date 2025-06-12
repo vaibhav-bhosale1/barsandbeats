44f02ee714391e122409dab8a62ab77298a6b354
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-06-12 20:48:27 +0530

    disabled eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+  extends: ['next', 'next/core-web-vitals'],
+  rules: {
+    'no-console': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    // Add other rules you want to disable
+  },
+};
