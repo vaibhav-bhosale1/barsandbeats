859ccfc9daa102d05d5c452c54e820761a848ff5
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-06-12 22:33:53 +0530

    fixed eslint conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,19 @@ module.exports = {
     'no-console': 'off',
     'react-hooks/exhaustive-deps': 'off',
     // Add other rules you want to disable
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
+    ]
+  ,
+  "ignorePatterns": [
+    "lib/generated/**/*",
+    "prisma/generated/**/*",
+    "node_modules/**/*",
+    ".next/**/*"
+  ]
   },
 };
